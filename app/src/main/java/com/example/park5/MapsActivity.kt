febde9e1a4d553b1b64bc6b8da2f8d7aeaef418a
Yash Dhanore <yashdhanore@gmail.com>
2021-12-11 16:37:49 +0100

    cleared some things and made it presentable

--- app/src/main/java/com/example/park5/MapsActivity.kt
+++ app/src/main/java/com/example/park5/MapsActivity.kt
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
@@ -85,6 +86,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
         private const val REQUEST_CHECK_SETTINGS = 2
+        private const val PARKING_API_KEY = "693167d9-7ce5-437a-90fd-030343a3bacf"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -112,7 +114,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         draw.addDrawerListener(toggle)
         toggle.syncState()
 
-
         //clicking on park5 logo on top goes back to main screen, basically creates a new activity
         val iv = findViewById<ImageView>(R.id.logocenter)
         iv.setOnClickListener(View.OnClickListener {
@@ -120,34 +121,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             startActivity(intent)
         })
 
-
-
         //getting map fragment & Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
-
         val bottomnav = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        //val bottomnav_right = findViewById<BottomNavigationView>(R.id.bottom_nav_right)
-        //bottomnav.menu.getItem(0).isCheckable = false
-        //bottomnav_right.menu.getItem(0).isCheckable = false
+        bottomnav.isHapticFeedbackEnabled = true
+        bottomnav.menu.getItem(0).isChecked = false
 
-
-        //for the center button
-        // @TODO fix this
-        bottomnav.itemIconTintList = null
-        val front = findViewById<ImageView>(R.id.tofront)
-        front.bringToFront()
-
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
-                lastLocation = p0.lastLocation
-            }
-        }
-
-        //bottomnav seletor
         BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
             when (item.itemId) {
 
@@ -190,6 +172,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
         }
 
+        //for the center button
+        // @TODO fix this
+        bottomnav.itemIconTintList = null
+        val front = findViewById<ImageView>(R.id.tofront)
+        front.bringToFront()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+            }
+        }
+
         //press button to find spots and add markers
         var fab: View = findViewById(R.id.parking)
         fab.setOnClickListener { view ->
@@ -222,40 +217,41 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }// End of OnCreate
 
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container,fragment)
+            transaction.commit()
+        }
+    }
+
     private fun startLocationUpdates() {
-        //1
         if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
-        //2
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */)
     }
 
     private fun createLocationRequest() {
-        // 2
         locationRequest.interval = 10000
-        // 3
         locationRequest.fastestInterval = 5000
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
 
         val builder = LocationSettingsRequest.Builder()
             .addLocationRequest(locationRequest)
 
-        // 4
         val client = LocationServices.getSettingsClient(this)
         val task = client.checkLocationSettings(builder.build())
 
-        // 5
         task.addOnSuccessListener {
             locationUpdateState = true
             startLocationUpdates()
         }
         task.addOnFailureListener { e ->
-            // 6
             if (e is ResolvableApiException) {
                 // Location settings are not satisfied, but this can be fixed
                 // by showing the user a dialog.
@@ -271,7 +267,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
-    // 1
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CHECK_SETTINGS) {
@@ -282,13 +277,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
-    // 2
     override fun onPause() {
         super.onPause()
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
-    // 3
     public override fun onResume() {
         super.onResume()
         if (!locationUpdateState) {
@@ -296,12 +289,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         }
     }
 
-
     private fun setUpMap() {
         if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
 
@@ -312,22 +304,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             if (location != null) {
                 lastLocation = location
                 currentLatLng = LatLng(location.latitude, location.longitude)
-                //placeMarkerOnMap(currentLatLng)
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
             }
         }
     }
     private fun getAddress(latLng: LatLng): String {
-        // 1
         val geocoder = Geocoder(this)
         val addresses: List<Address>?
         val address: Address?
         var addressText = ""
 
         try {
-            // 2
             addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
-            // 3
             if (null != addresses && !addresses.isEmpty()) {
                 address = addresses[0]
                 for (i in 0 until address.maxAddressLineIndex) {
@@ -341,6 +329,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         return addressText
     }
 
+    fun setCheckable(view: BottomNavigationView, checkable: Boolean) {
+        val menu: Menu = view.menu
+        for (i in 0 until menu.size()) {
+            menu.getItem(i).setCheckable(checkable)
+        }
+    }
 
     private fun placeMarkerOnMap(location: LatLng) {
         var markerOptions = MarkerOptions().position(location).icon(
@@ -354,12 +348,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         val titleStr = getAddress(location)  // add these two lines
         markerOptions.title("here")
         mMap.addMarker(markerOptions)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15F))
 
     }
 
     private fun findPos(callback: (ArrayList<ArrayList<Double>>?) -> Unit) {
         val service = GetObject.retrofitInstance?.create(GetInterface::class.java)
-        val call = service?.getPost(100,currentLatLng.latitude,currentLatLng.longitude,"json","693167d9-7ce5-437a-90fd-030343a3bacf")
+        val call = service?.getPost(100,currentLatLng.latitude,currentLatLng.longitude,"json",
+            PARKING_API_KEY)
         call?.enqueue(object : Callback<Get> {
             override fun onResponse(call: Call<Get>, response: Response<Get>) {
                 var body = response.body()
